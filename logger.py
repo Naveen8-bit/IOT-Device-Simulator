9ec40a8b91e73a00493e9aef21527028483e608e
Naveen <126980259+Naveen8-bit@users.noreply.github.com>
2026-04-08 16:56:50 +0530

    logger.py

--- logger.py
+++ logger.py
@@ -1,14 +1,29 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 def setup_logger():
+    logger = logging.getLogger("iot_device")
+    logger.setLevel(logging.INFO)
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        handlers=[
-            logging.FileHandler("device.log"),  
-            logging.StreamHandler()             
-        ]
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    file_handler = RotatingFileHandler(
+        "device.log",
+        maxBytes=1_000_000,
+        backupCount=3
     )
 
-    return logging
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
