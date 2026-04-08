40b9754908c54919f00ff5d717279ccba5d782f9
Naveen <126980259+Naveen8-bit@users.noreply.github.com>
2026-04-08 16:53:56 +0530

    sensor.py

--- sensor.py
+++ sensor.py
@@ -1,6 +1,15 @@
 import random
-class TemperatureSensor:
+
+class Sensor:
     def read_temperature(self):
-        if random.random() < 0.1:
-            raise Exception("Sensor failure")
-        return random.randint(-40, 80)
+        if random.random() < 0.01:
+            raise Exception("Temperature sensor failure")
+        return random.randint(20, 40)
+
+    def read_humidity(self):
+        if random.random() < 0.01:
+            raise Exception("Humidity sensor failure")
+        return random.randint(40, 80)
+
+    def get_status(self):
+        return random.choice(["OK", "WARN", "FAIL"])
